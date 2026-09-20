db0057a04d9ac0a2a96449ffeec3d80698580997
grgrsm <grigorisem07@gmail.com>
2026-09-21 01:33:42 +0500

    name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   root: '.',
-  base: '/minigames/',
+  base: '/rsschool-minigames/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
