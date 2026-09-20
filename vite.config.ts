649c61e0e513c905e2034896e4d3447055d3b547
grgrsm <grigorisem07@gmail.com>
2026-09-21 01:31:28 +0500

    icons

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   root: '.',
-  base: './',
+  base: '/minigames/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
